50e4bdddc89d4f7b89c37994944ea7a3a4b00fd9
Javier Abella <abeljvrg@gmail.com>
2020-05-19 19:12:23 +0200

    Update url paths

--- webempresa/urls.py
+++ webempresa/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+if settings.DEBUG is True:
+    from django.conf.urls.static import static
 
-from core import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('services/', include('services.urls')),
 ]
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
