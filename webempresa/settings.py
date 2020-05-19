e8aa31fd18150afb8540c82044933afa08a2a28d
Javier Abella <abeljvrg@gmail.com>
2020-05-19 19:13:30 +0200

    Update setting to show media files

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'services.apps.ServicesConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Media Files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
