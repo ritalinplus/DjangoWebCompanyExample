3dd82655ebc91982856b1d6d451f7715aab94d04
Javier Abella <abeljvrg@gmail.com>
2020-05-29 12:50:09 +0200

    Add blog and page urls

--- webempresa/urls.py
+++ webempresa/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('services/', include('services.urls')),
+    path('blog/', include('blog.urls')),
+    path('page/', include('pages.urls')),
 ]
 
 if settings.DEBUG is True:
