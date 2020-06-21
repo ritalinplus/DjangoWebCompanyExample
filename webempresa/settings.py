19cbf1edadbb15654c018375479f71525303badb
Javier Abella <abeljvrg@gmail.com>
2020-06-21 11:34:19 +0200

    Add path to static files dirs

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'webempresa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,6 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Media Files
 MEDIA_URL = '/media/'
